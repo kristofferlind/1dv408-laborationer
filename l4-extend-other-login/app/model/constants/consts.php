11234d4c2a3c040906cc13e9bd3c4a2c23759d2d
kristofferlind <kristoffer@krad.se>
2014-09-23 09:13:52 +0200

    new consts.php

---
+++ l4-extend-other-login/app/model/constants/consts.php
@@ -0,0 +1,9 @@
+<?php
+
+// hacking prevention
+if (!defined('inlogg_def')) die('missing context');
+
+define('c_Con_Host', 'localhost');
+define('c_Con_User', 'root');
+define('c_Con_PW', '');
+define('c_Con_DB', 'inlogg');
