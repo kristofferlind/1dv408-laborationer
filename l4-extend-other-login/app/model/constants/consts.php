c93dc0b79dc5c066bc1c148650e8e6717a61848b
Kristoffer Lind <kristoffer@krad.se>
2014-09-23 09:08:55 +0200

    Delete consts.php

--- l4-extend-other-login/app/model/constants/consts.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// hacking prevention
-if (!defined('inlogg_def')) die('missing context');
-
-define('c_Con_Host', '1dv408-l4-117336.mysql.binero.se'); //1dv408-l4-117336.mysql.binero.se, localhost
-define('c_Con_User', '117336_kr12843'); //117336_kr12843, root, winwerdin_com
-define('c_Con_PW', 'N8169k0g1M0t93K'); //N8169k0g1M0t93K, '', drachlyn2
-define('c_Con_DB', '117336-1dv408-l4'); //117336-1dv408-l4, inlogg, winwerdin_com
